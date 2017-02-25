76be77387045e066f9a2d431c8b1a060e1744911
marcodondio <marcodondio@192.168.1.71>
2017-02-25 18:09:49 +0100

    Aggiornato servlet ciao

--- src/prova/Ciao.java
+++ src/prova/Ciao.java
@@ -27,7 +27,7 @@ public class Ciao extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Ciao povero, served at: ").append(request.getContextPath());
 	}
 
 	/**
